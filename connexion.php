027363504704712bd76909e91721af3bdf119651
dadaa2 <urisonten@gmail.com>
2023-12-08 14:52:18 +0100

    Suppression de index2.css

--- connexion.php
+++ connexion.php
@@ -10,9 +10,9 @@
                             $mdp, 
                             $base_de_donnees);
     
-    if($conn){
-        echo"<footer> Connexion au base de donnée établie ! <br></footer>";
-    }
-    else{
-        echo "Connexion au base de données ne s'est pas effectué !";}
+    //if($conn){
+    //    echo"<footer> Connexion au base de donnée établie ! <br></footer>";
+    //}
+    //else{
+    //    echo "Connexion au base de données ne s'est pas effectué !";}
 ?>
--- reservation.php
+++ reservation.php
@@ -88,7 +88,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Réservation</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="reservation.css">
 </head>
 <body>
     <h1>Réservation</h1>
